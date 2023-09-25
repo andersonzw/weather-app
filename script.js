61e8f7ee73de9350eb8b8615f4418400c45d8879
Anderson <84068667+andersonzw@users.noreply.github.com>
2023-09-25 14:18:56 +1000

    added most functionalities

--- script.js
+++ script.js
@@ -2,14 +2,49 @@
 
 // http://api.weatherapi.com/v1/current.json?key=d00946e814a4480e8c020844232509&q=London
 
-const text = document.querySelector(".display")
 
-async function getWeather() {
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=d00946e814a4480e8c020844232509&q=London')
+
+
+async function getWeather(searchInput) {
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=d00946e814a4480e8c020844232509&q=${searchInput}`)
     const weatherData = await response.json();
-    console.log(response);
-    console.log({weatherData});
-    console.log(weatherData.current.feelslike_c);
+
+    const data = {
+        "city" : weatherData.location.name,
+        "currentTemp" : weatherData.current.temp_c,
+        "humidity" : weatherData.current.humidity,
+        "condition" : weatherData.current.condition.text,
+        "windspeed" : weatherData.current.wind_kph,
+        "iconsrc" : weatherData.current.condition.icon
+    }
+    console.log(data);
+
+    document.querySelector(".city").textContent = `Weather in ${data.city}`
+    document.querySelector(".temp").textContent = `${data.currentTemp}°C`
+    document.querySelector(".description").textContent = `${data.condition}`
+    document.querySelector(".icon").src = data.iconsrc
+    document.querySelector(".humidity").textContent = `Humidity: ${data.humidity}%`
+    document.querySelector(".wind").textContent = `Wind Speed: ${data.windspeed}kph`
+    
+    console.log("object");
+    document.querySelector(".card-content").classList.remove("loading")
 }
 
-getWeather()
+
+const searchBar = document.querySelector(".search-bar")
+const searchButton = document.querySelector("button")
+
+searchButton.addEventListener("click", ()=> {
+    getWeather(searchBar.value)
+})
+
+searchBar.addEventListener("keypress", (e)=> {
+    if (e.code === 'Enter') {
+        getWeather(searchBar.value)
+    }
+
+})
+
+
+getWeather("melbourne")
+
